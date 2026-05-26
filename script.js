90f5fa3f6e61dfd3178b6dafe01a6b18d0ff800d
Aro_Samson <arosamson07@gmail.com>
2026-05-26 13:27:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// ----------------typing------------------
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Full Stack Developer', 'Computer Science Student'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+// -----------------------Scroll reveal------------------
+
+ScrollReveal({
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-text, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-image, .project-container, .certificate-container', { origin: 'bottom' });
+ScrollReveal().reveal('.about-img, .technical-skill, .contact-left', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content, .tool-box, .contact-right', { origin: 'right' });
